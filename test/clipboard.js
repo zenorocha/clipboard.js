ec20389775e67b070b5b350b6ba137e8562a71c2
Zeno Rocha <hi@zenorocha.com>
2015-09-20 00:57:19 -0700

    Adds tests for constructor

--- test/clipboard.js
+++ test/clipboard.js
@@ -1,3 +1,28 @@
-describe('Clipboard', () => {
+var assert = chai.assert;
 
+describe('Clipboard', () => {
+    describe('#constructor', () => {
+        it('should throw an error since there was no arguments passed', done => {
+            try {
+                new Clipboard();
+            }
+            catch(e) {
+                done();
+            }
+        });
+
+        it('should throw an error since an empty selector has been passed', done => {
+            try {
+                new Clipboard('#abc');
+            }
+            catch(e) {
+                done();
+            }
+        });
+
+        it('should create a NodeList and store it in a property', () => {
+            let clipboard = new Clipboard('.btn');
+            return assert.instanceOf(clipboard.triggers, NodeList);
+        });
+    });
 });
