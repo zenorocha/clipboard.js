cb08fd826f36df1627556a2782d75ea93e56d62c
Zeno Rocha <hi@zenorocha.com>
2015-09-23 09:56:05 -0700

    Clear selection only when operation has succeeded

--- assets/main.js
+++ assets/main.js
@@ -6,26 +6,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
     for (var i = 0; i < btns.length; i++) {
         btns[i].addEventListener('success', function(e) {
+            showTooltip(e.currentTarget, 'Copied!');
+
             console.info('Action:', e.detail.action);
             console.info('Text:', e.detail.text);
-
-            e.currentTarget.classList.add('tooltipped', 'tooltipped-s');
-            e.currentTarget.setAttribute('aria-label', 'Copied!');
-        });
-
-        btns[i].addEventListener('mouseleave', function(e) {
-            e.currentTarget.classList.remove('tooltipped', 'tooltipped-s');
-            e.currentTarget.removeAttribute('aria-label');
         });
 
         btns[i].addEventListener('error', function(e) {
-            var flashes = document.querySelectorAll('.flash');
+            if (e.currentTarget.hasAttribute('data-text')) {
+                var flash = document.querySelector('.flash');
+                flash.textContent = e.detail;
+                flash.setAttribute('class', 'flash flash-error');
 
-            for (var j = 0; j < btns.length; j++) {
-                btns[j].disabled = true;
-                flashes[j].textContent = e.detail;
-                flashes[j].classList.remove('hidden');
+                e.currentTarget.disabled = true;
+            }
+            else {
+                showTooltip(e.currentTarget, 'Selected!');
             }
         });
+    }
+
+    function showTooltip(elem, msg) {
+        elem.setAttribute('class', 'btn tooltipped tooltipped-s');
+        elem.setAttribute('aria-label', msg);
     }
 });
