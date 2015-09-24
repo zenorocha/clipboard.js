1febe4eeccb6e8be33e864b2c404c97d5dfcdcac
Zeno Rocha <hi@zenorocha.com>
2015-09-24 15:11:56 -0700

    Adds karma test suite

---
+++ test/clipboard-action.js
@@ -0,0 +1,42 @@
+import ClipboardAction from '../src/clipboard-action';
+
+describe('ClipboardAction', () => {
+    before(() => {
+        global.target = document.createElement('input');
+        target.setAttribute('id', 'foo');
+        document.body.appendChild(global.target);
+
+        global.trigger = document.createElement('button');
+        trigger.setAttribute('class', 'btn');
+        document.body.appendChild(global.trigger);
+    });
+
+    describe('#constructor', () => {
+        it('should throw an error since "data-action" is invalid', (done) => {
+            try {
+                new Clipboard({
+                    action: 'paste'
+                });
+            }
+            catch(e) {
+                done();
+            }
+        });
+
+        it('should throw an error since "data-target" do not match any element', (done) => {
+            try {
+                new ClipboardAction({
+                    target: 'zzz',
+                    trigger: global.trigger
+                });
+            }
+            catch(e) {
+                done();
+            }
+        });
+    });
+
+    after(() => {
+        document.body.innerHTML = '';
+    });
+});
--- test/clipboard.js
+++ test/clipboard.js
@@ -1,6 +1,12 @@
-var assert = chai.assert;
+import Clipboard from '../src/clipboard';
 
 describe('Clipboard', () => {
+    before(() => {
+        let button = document.createElement('button');
+        button.setAttribute('class', 'btn');
+        document.body.appendChild(button);
+    });
+
     describe('#constructor', () => {
         it('should throw an error since there was no arguments passed', done => {
             try {
@@ -20,51 +26,13 @@ describe('Clipboard', () => {
             }
         });
 
-        it('should create a NodeList and store it in a property', () => {
+        it('should create a NodeList from selector and store it in a "triggers" property', () => {
             let clipboard = new Clipboard('.btn');
             return assert.instanceOf(clipboard.triggers, NodeList);
         });
     });
 
-    describe('#validate', () => {
-        var elem = document.createElement('input');
-        let clipboard = new Clipboard('.btn');
-
-        it('should throw an error since there was no "data-target" or "data-text"', (done) => {
-            try {
-                clipboard.validate({
-                    currentTarget: elem
-                });
-            }
-            catch(e) {
-                done();
-            }
-        });
-
-        it('should throw an error since "data-action" is invalid', (done) => {
-            try {
-                elem.setAttribute('data-action', 'paste');
-
-                clipboard.validate({
-                    currentTarget: elem
-                });
-            }
-            catch(e) {
-                done();
-            }
-        });
-
-        it('should throw an error since "data-target" do not match any element', (done) => {
-            try {
-                elem.setAttribute('data-target', 'xyz');
-
-                clipboard.validate({
-                    currentTarget: elem
-                });
-            }
-            catch(e) {
-                done();
-            }
-        });
+    after(() => {
+        document.body.innerHTML = '';
     });
 });
