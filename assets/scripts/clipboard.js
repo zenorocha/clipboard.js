b1f3dfe95193916612c92e93d74005376db638f5
Zeno Rocha <hi@zenorocha.com>
2015-09-25 00:19:31 -0700

    Updates documentation

--- assets/scripts/clipboard.js
+++ assets/scripts/clipboard.js
@@ -5,10 +5,14 @@ clipboard.on('success', function(e) {
 
     console.info('Action:', e.action);
     console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
 
     showTooltip(e.trigger, 'Copied!');
 });
 
 clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+
     showTooltip(e.trigger, fallbackMessage(e.action));
 });
