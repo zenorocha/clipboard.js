705e2dbefdf40911ee2fe266a514b982d3edd0b6
Zeno Rocha <hi@zenorocha.com>
2015-10-24 11:41:39 -0700

    Release v1.4.3

--- dist/clipboard.js
+++ dist/clipboard.js
@@ -1,62 +1,10 @@
 /*!
- * clipboard.js v1.4.2
+ * clipboard.js v1.4.3
  * https://zenorocha.github.io/clipboard.js
  *
  * Licensed MIT © Zeno Rocha
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Clipboard = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/**
- * Module dependencies.
- */
-
-var closest = require('closest')
-  , event = require('component-event');
-
-/**
- * Delegate event `type` to `selector`
- * and invoke `fn(e)`. A callback function
- * is returned which may be passed to `.unbind()`.
- *
- * @param {Element} el
- * @param {String} selector
- * @param {String} type
- * @param {Function} fn
- * @param {Boolean} capture
- * @return {Function}
- * @api public
- */
-
-// Some events don't bubble, so we want to bind to the capture phase instead
-// when delegating.
-var forceCaptureEvents = ['focus', 'blur'];
-
-exports.bind = function(el, selector, type, fn, capture){
-  if (forceCaptureEvents.indexOf(type) !== -1) capture = true;
-
-  return event.bind(el, type, function(e){
-    var target = e.target || e.srcElement;
-    e.delegateTarget = closest(target, selector, true, el);
-    if (e.delegateTarget) fn.call(el, e);
-  }, capture);
-};
-
-/**
- * Unbind event `type`'s callback `fn`.
- *
- * @param {Element} el
- * @param {String} type
- * @param {Function} fn
- * @param {Boolean} capture
- * @api public
- */
-
-exports.unbind = function(el, type, fn, capture){
-  if (forceCaptureEvents.indexOf(type) !== -1) capture = true;
-
-  event.unbind(el, type, fn, capture);
-};
-
-},{"closest":2,"component-event":4}],2:[function(require,module,exports){
 var matches = require('matches-selector')
 
 module.exports = function (element, selector, checkYoSelf) {
@@ -68,7 +16,7 @@ module.exports = function (element, selector, checkYoSelf) {
   }
 }
 
-},{"matches-selector":3}],3:[function(require,module,exports){
+},{"matches-selector":2}],2:[function(require,module,exports){
 
 /**
  * Element prototype.
@@ -109,42 +57,64 @@ function match(el, selector) {
   }
   return false;
 }
-},{}],4:[function(require,module,exports){
-var bind = window.addEventListener ? 'addEventListener' : 'attachEvent',
-    unbind = window.removeEventListener ? 'removeEventListener' : 'detachEvent',
-    prefix = bind !== 'addEventListener' ? 'on' : '';
+},{}],3:[function(require,module,exports){
+var closest = require('closest');
 
 /**
- * Bind `el` event `type` to `fn`.
+ * Delegate event `type` to `selector`
+ * and invoke `fn(e)`. A callback function
+ * is returned which may be passed to `.unbind()`.
  *
  * @param {Element} el
+ * @param {String} selector
  * @param {String} type
  * @param {Function} fn
  * @param {Boolean} capture
  * @return {Function}
- * @api public
  */
 
-exports.bind = function(el, type, fn, capture){
-  el[bind](prefix + type, fn, capture || false);
-  return fn;
+exports.bind = function(el, selector, type, fn, capture){
+    return el.addEventListener(type, function(e){
+        var target = e.target || e.srcElement;
+        e.delegateTarget = closest(target, selector, true, el);
+        if (e.delegateTarget) fn.call(el, e);
+    }, capture);
 };
 
 /**
- * Unbind `el` event `type`'s callback `fn`.
+ * Unbind event `type`'s callback `fn`.
  *
  * @param {Element} el
  * @param {String} type
  * @param {Function} fn
  * @param {Boolean} capture
- * @return {Function}
- * @api public
  */
 
 exports.unbind = function(el, type, fn, capture){
-  el[unbind](prefix + type, fn, capture || false);
-  return fn;
+    el.removeEventListener(type, fn, capture);
 };
+
+},{"closest":1}],4:[function(require,module,exports){
+function select(element) {
+    var selection = window.getSelection();
+
+    if (element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA') {
+        element.selectionStart = 0;
+        element.selectionEnd = element.value.length;
+    }
+    else {
+        var range = document.createRange();
+
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+
+    return selection.toString();
+}
+
+module.exports = select;
+
 },{}],5:[function(require,module,exports){
 function E () {
 	// Keep this empty so it's easier to inherit from
@@ -154,23 +124,24 @@ function E () {
 E.prototype = {
 	on: function (name, callback, ctx) {
     var e = this.e || (this.e = {});
-    
+
     (e[name] || (e[name] = [])).push({
       fn: callback,
       ctx: ctx
     });
-    
+
     return this;
   },
 
   once: function (name, callback, ctx) {
     var self = this;
-    var fn = function () {
-      self.off(name, fn);
+    function listener () {
+      self.off(name, listener);
       callback.apply(ctx, arguments);
     };
-    
-    return this.on(name, fn, ctx);
+
+    listener._ = callback
+    return this.on(name, listener, ctx);
   },
 
   emit: function (name) {
@@ -178,11 +149,11 @@ E.prototype = {
     var evtArr = ((this.e || (this.e = {}))[name] || []).slice();
     var i = 0;
     var len = evtArr.length;
-    
+
     for (i; i < len; i++) {
       evtArr[i].fn.apply(evtArr[i].ctx, data);
     }
-    
+
     return this;
   },
 
@@ -190,21 +161,22 @@ E.prototype = {
     var e = this.e || (this.e = {});
     var evts = e[name];
     var liveEvents = [];
-    
+
     if (evts && callback) {
       for (var i = 0, len = evts.length; i < len; i++) {
-        if (evts[i].fn !== callback) liveEvents.push(evts[i]);
+        if (evts[i].fn !== callback && evts[i].fn._ !== callback)
+          liveEvents.push(evts[i]);
       }
     }
-    
+
     // Remove event from queue to prevent memory leak
     // Suggested by https://github.com/lazd
     // Ref: https://github.com/scottcorgan/tiny-emitter/commit/c6ebfaa9bc973b33d110a84a307742b7cf94c953#commitcomment-5024910
 
-    (liveEvents.length) 
+    (liveEvents.length)
       ? e[name] = liveEvents
       : delete e[name];
-    
+
     return this;
   }
 };
@@ -212,17 +184,24 @@ E.prototype = {
 module.exports = E;
 
 },{}],6:[function(require,module,exports){
-/**
- * Inner class which performs selection from either `text` or `target`
- * properties and then executes copy or cut operations.
- */
 'use strict';
 
 exports.__esModule = true;
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _select = require('select');
+
+var _select2 = _interopRequireDefault(_select);
+
+/**
+ * Inner class which performs selection from either `text` or `target`
+ * properties and then executes copy or cut operations.
+ */
 
 var ClipboardAction = (function () {
     /**
@@ -290,11 +269,10 @@ var ClipboardAction = (function () {
         this.fakeElem.style.top = (window.pageYOffset || document.documentElement.scrollTop) + 'px';
         this.fakeElem.setAttribute('readonly', '');
         this.fakeElem.value = this.text;
-        this.selectedText = this.text;
 
         document.body.appendChild(this.fakeElem);
 
-        this.fakeElem.select();
+        this.selectedText = _select2['default'](this.fakeElem);
         this.copyText();
     };
 
@@ -320,19 +298,7 @@ var ClipboardAction = (function () {
      */
 
     ClipboardAction.prototype.selectTarget = function selectTarget() {
-        if (this.target.nodeName === 'INPUT' || this.target.nodeName === 'TEXTAREA') {
-            this.target.select();
-            this.selectedText = this.target.value;
-        } else {
-            var range = document.createRange();
-            var selection = window.getSelection();
-
-            selection.removeAllRanges();
-            range.selectNodeContents(this.target);
-            selection.addRange(range);
-            this.selectedText = selection.toString();
-        }
-
+        this.selectedText = _select2['default'](this.target);
         this.copyText();
     };
 
@@ -451,7 +417,7 @@ var ClipboardAction = (function () {
 exports['default'] = ClipboardAction;
 module.exports = exports['default'];
 
-},{}],7:[function(require,module,exports){
+},{"select":4}],7:[function(require,module,exports){
 'use strict';
 
 exports.__esModule = true;
@@ -466,9 +432,9 @@ var _clipboardAction = require('./clipboard-action');
 
 var _clipboardAction2 = _interopRequireDefault(_clipboardAction);
 
-var _delegateEvents = require('delegate-events');
+var _delegate = require('delegate');
 
-var _delegateEvents2 = _interopRequireDefault(_delegateEvents);
+var _delegate2 = _interopRequireDefault(_delegate);
 
 var _tinyEmitter = require('tiny-emitter');
 
@@ -524,7 +490,7 @@ var Clipboard = (function (_Emitter) {
     Clipboard.prototype.delegateClick = function delegateClick(selector) {
         var _this = this;
 
-        this.binding = _delegateEvents2['default'].bind(document.body, selector, 'click', function (e) {
+        this.binding = _delegate2['default'].bind(document.body, selector, 'click', function (e) {
             return _this.onClick(e);
         });
     };
@@ -535,7 +501,7 @@ var Clipboard = (function (_Emitter) {
      */
 
     Clipboard.prototype.undelegateClick = function undelegateClick() {
-        _delegateEvents2['default'].unbind(document.body, 'click', this.binding);
+        _delegate2['default'].unbind(document.body, 'click', this.binding);
     };
 
     /**
@@ -617,5 +583,5 @@ function getAttributeValue(suffix, element) {
 exports['default'] = Clipboard;
 module.exports = exports['default'];
 
-},{"./clipboard-action":6,"delegate-events":1,"tiny-emitter":5}]},{},[7])(7)
+},{"./clipboard-action":6,"delegate":3,"tiny-emitter":5}]},{},[7])(7)
 });
