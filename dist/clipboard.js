402c9ee17bed6f273bcbc7efa81874fd9a50b84c
Zeno Rocha <zno.rocha@gmail.com>
2016-12-12 11:54:35 -0200

    Release v1.5.16

--- dist/clipboard.js
+++ dist/clipboard.js
@@ -1,5 +1,5 @@
 /*!
- * clipboard.js v1.5.15
+ * clipboard.js v1.5.16
  * https://zenorocha.github.io/clipboard.js
  *
  * Licensed MIT © Zeno Rocha
--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: "zenorocha:clipboard",
   summary: "Modern copy to clipboard. No Flash. Just 2kb.",
-  version: "1.5.15",
+  version: "1.5.16",
   git: "https://github.com/zenorocha/clipboard.js"
 });
 
