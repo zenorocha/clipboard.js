85981026d1f464c9f645ffe543a3ba0d40c9b6c4
Christian Oliff <christianoliff@yahoo.com>
2019-02-22 23:30:35 +0900

    update URL in banner

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const production = process.env.NODE_ENV === 'production' || false;
 
 const banner = `clipboard.js v${pkg.version}
-https://zenorocha.github.io/clipboard.js
+https://clipboardjs.com/
 
 Licensed MIT © Zeno Rocha`;
 
