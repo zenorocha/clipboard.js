fddd2aac5f8772c468b36e8607ab9b0704ee73c6
Zeno Rocha <zno.rocha@gmail.com>
2020-03-04 22:26:34 -0800

    2.0.6

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: "zenorocha:clipboard",
   summary: "Modern copy to clipboard. No Flash. Just 3kb.",
-  version: "2.0.5",
+  version: "2.0.6",
   git: "https://github.com/zenorocha/clipboard.js"
 });
 
