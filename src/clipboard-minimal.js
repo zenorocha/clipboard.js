72e7b7f13028844deb5bc8eb9b03605f700b600c
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-01-19 14:54:49 -0300

    Minimal interface of copy

---
+++ src/clipboard-minimal.js
@@ -0,0 +1,31 @@
+function clipboard() {
+  return {
+    copy: (input) => {
+      const isRTL = document.documentElement.getAttribute('dir') == 'rtl';
+
+      const fakeElem = document.createElement('textarea');
+      // Prevent zooming on iOS
+      fakeElem.style.fontSize = '12pt';
+      // Reset box model
+      fakeElem.style.border = '0';
+      fakeElem.style.padding = '0';
+      fakeElem.style.margin = '0';
+      // Move element out of screen horizontally
+      fakeElem.style.position = 'absolute';
+      fakeElem.style[ isRTL ? 'right' : 'left' ] = '-9999px';
+      // Move element to the same position vertically
+      let yPosition = window.pageYOffset || document.documentElement.scrollTop;
+      fakeElem.style.top = `${yPosition}px`;
+
+      fakeElem.setAttribute('readonly', '');
+      fakeElem.value = input;
+
+      document.body.append(fakeElem);
+
+      fakeElem.select();
+
+      return document.execCommand('copy');
+    }
+  };
+}
+export default clipboard();
