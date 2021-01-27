e0cb36b12b5102d79643c54102537e7aeb751938
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-01-27 17:05:25 -0300

    Minimal usage of the api

--- test/clipboard-minimal.js
+++
@@ -1,7 +0,0 @@
-import clipboard from '../src/clipboard-minimal';
-
-describe('Clipboard minimal api', () => {
-  it('should have copy', () => {
-    assert.ok(clipboard.copy !== undefined);
-  });
-});
